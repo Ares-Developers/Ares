052d33d72ef0b03d76fa2d4a95ba9907e4a0b49c
pd <pdmail@gmx.de>
2008-08-15 19:52:42 +0000

    Updated the temporal fix to the Wiki version.
    git-svn-id: svn://svn.renegadeprojects.com/ares/trunk@3 859b54a9-7a54-0410-aeb3-f8d2f1fa40fd

--- fix_temporal/fix_temporal.cpp
+++ fix_temporal/fix_temporal.cpp
@@ -1,9 +1,7 @@
 //TEST DLL
 //=============
 //
-
-#define UNICODE
-
+// keep this part in
 #ifndef _CRT_SECURE_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS
 #endif
@@ -12,26 +10,30 @@
 #endif
 #pragma warning(disable: 4035)	//"no return value" - there is one, just not in our code ;)
 
+// standard C++ headers
 #include <stdio.h>
-#include "../ASMMacros.h"
-#include "../YRPP.h"
-#include "../ArrayClasses.h"
-//include "../Drawing.cpp"
-//include "../MessageBox.cpp"
-//include "../CommandClass.h"
-//include "../StringTable.cpp"
-#include "../Debugger.cpp"
 
-// 0x71A92A - Temporal veterancy
-EXPORT _TEST_AvoidFriendlies() {
-	TemporalClass *m = (TemporalClass *)Debugger::GetReg32(rESI);
+// YR++ library
+#include <YRPP.h>
+
+// the actual function 
+
+//hook at 0x71A92A
+EXPORT _Temporal_AvoidFriendlies(REGISTERS *R) {
+	// (ASM knowledge) the ESI register contains a pointer to an instance of TemporalClass, let's get it
+	TemporalClass *m = (TemporalClass *)R->get_ESI(); 
+
+	// House that owns the unit firing this weapon
 	HouseClass *hv = m->get_TargetUnit()->get_Owner();
+
+	// House that owns the unit being erased
 	HouseClass *ho = m->get_OwningUnit()->get_Owner();
 
+	// if the two houses aren't allied with each other, there's nothing to fix
 	if(!ho->IsAlliedWith(hv)) {
-		return;
+		return 0;
 	}
-//	Mouse gets frozen on last cursor, needs fixing
-//	MessageBox::Show(L"No cheating!", L"OK", L"OK");
-	Debugger::SetReturnEIP(0x71A97D);
+
+	// (ASM knowledge) force the game to jump over the experience-granting code
+	return 0x71A97D;
 }
