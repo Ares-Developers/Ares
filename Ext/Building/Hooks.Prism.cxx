96433d978e0e884d170a3fc0d43942a2c6dc734c
DCoder <dcoder1337@gmail.com>
2010-05-13 20:08:56 +0000

    More code for prisms. Related to issue 340 .
    git-svn-id: svn://svn.renegadeprojects.com/ares/trunk@522 859b54a9-7a54-0410-aeb3-f8d2f1fa40fd

--- Ext/Building/Hooks.Prism.cxx
+++ Ext/Building/Hooks.Prism.cxx
@@ -9,6 +9,60 @@ DEFINE_HOOK(44B2FE, BuildingClass_Mi_Attack_IsPrism, 6)
 	// IsPrism - will find a single support Westwood style and apply the effects to it
 	// IsCustomPrism - Ares code is used to find supports manually and TODO pseudocode all needed stuff
 	enum { IsPrism = 0x44B310, IsNotPrism = 0x44B630, IsCustomPrism = 0x44B6D6};
+
+	if(B->Type == RulesClass::Instance->PrismType) {
+
+		// find a slave
+		int nearestDistance = 0x7FFFFFFF;
+		BuildingClass * nearestPrism = NULL;
+
+		CoordStruct MyPosition, curPosition;
+
+		B->GetPosition_2(&MyPosition);
+
+		int Range = B->GetWeaponRange(1);
+
+		for(int i = 0; i < B->Owner->Buildings.Count; ++i) {
+			if(BuildingClass * curBld = B->Owner->Buildings[i]) {
+				if(curBld->IsAlive && curBld->Type == RulesClass::Instance->PrismType) {
+					if(curBld->ReloadTimer.Ignorable()) {
+						if(curBld != B && !curBld->PrismReloadDelay) {
+							if(!curBld->IsBeingDrained() && curBld->GetCurrentMission() != mission_Attack) {
+								curBld->GetPosition_2(&curPosition);
+								int Distance = MyPosition.DistanceFrom(curPosition);
+								if(Distance <= Range) {
+									if(!nearestPrism || Distance < nearestDistance) {
+										nearestPrism = curBld;
+										nearestDistance = Distance;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		if(nearestPrism) {
+			++B->SupportingPrisms;
+			CoordStruct FLH, Base = {0, 0, 0};
+			B->GetFLH(&FLH, 0, Base);
+			nearestPrism->PrismReloadDelay = nearestPrism->Type->DelayedFireDelay;
+			nearestPrism->PrismStage = pcs_Slave;
+			nearestPrism->PrismTargetCoords = FLH;
+			nearestPrism->DestroyNthAnim(BuildingAnimSlot::Active);
+			nearestPrism->PlayNthAnim(BuildingAnimSlot::Special);
+		}
+
+		// set up master - it seems this happens (and resets the Delay) each time the function is triggered which is every 1 frame...
+		B->PrismReloadDelay = B->Type->DelayedFireDelay;
+		B->PrismStage = pcs_Master;
+		B->PrismTargetCoords.X = 0;
+		B->PrismTargetCoords.Y = B->PrismTargetCoords.Z = 0; // these are set to uninitialized vars really, but don't seem to be used for master
+		B->DestroyNthAnim(BuildingAnimSlot::Active);
+		B->PlayNthAnim(BuildingAnimSlot::Special);
+	}
+
 	return IsNotPrism;
 }
 
@@ -29,12 +83,12 @@ DEFINE_HOOK(4503F0, BuildingClass_Update_Prism, 9)
 	if(int PrismStage = pThis->PrismStage) {
 		--pThis->PrismReloadDelay;
 		if(pThis->PrismReloadDelay <= 0) {
-			if(PrismStage == pcs_Firing) {
+			if(PrismStage == pcs_Slave) {
 				pThis->FireLaser(pThis->PrismTargetCoords);
 				pThis->PrismStage = pcs_Idle;
 				return end;
 			}
-			if(PrismStage == pcs_Charging) {
+			if(PrismStage == pcs_Master) {
 				if(pThis->Target) {
 					if(pThis->GetFireError(pThis->Target, pThis->PrismTargetCoords.X, true) == FireError::OK) {
 						if(BulletClass *LaserBeam = pThis->Fire(pThis->Target, pThis->PrismTargetCoords.X)) {
