e7c24eb6f799469f65678b5fe79c559f65ad9408
AlexB <Alexander-B@gmx.de>
2010-05-19 03:19:14 +0000

    Show revision also. And fix the overlapping text.
    git-svn-id: svn://svn.renegadeprojects.com/ares/trunk@556 859b54a9-7a54-0410-aeb3-f8d2f1fa40fd

--- Ares.version.h
+++ Ares.version.h
@@ -3,7 +3,7 @@
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 1
-#define VERSION_REVISION 544
+#define VERSION_REVISION 556
 
 #define SAVEGAME_MAGIC ((VERSION_MAJOR << 24) | (VERSION_MINOR << 16) | (VERSION_REVISION))
 
@@ -11,7 +11,7 @@
 #define str_(x) #x
 
 #define VERSION_PREFIX "Yuri's Revenge 1.001 + Ares version "
-#define VERSION_STR str(VERSION_MAJOR) "." str(VERSION_MINOR)
+#define VERSION_STR str(VERSION_MAJOR) "." str(VERSION_MINOR) "." str(VERSION_REVISION)
 
 // "Yuri's Revenge 1.001 + Ares version: $ver"
 #define VERSION_STRING VERSION_PREFIX VERSION_STR
