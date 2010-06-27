8839e882b4da5849df8a8948f28e17a156e4622c
DCoder <dcoder1337@gmail.com>
2010-06-27 06:13:49 +0000

    <strong>Fixed issue #1026</strong> - removing one debug::log that's slowing things down, moving a hook to a more relevant place. Related to issue 1026 .
    git-svn-id: svn://svn.renegadeprojects.com/ares/trunk@707 859b54a9-7a54-0410-aeb3-f8d2f1fa40fd

--- src/Ext/House/Hooks.cpp
+++ src/Ext/House/Hooks.cpp
@@ -158,3 +158,35 @@ DEFINE_HOOK(4F62FF, HouseClass_CTOR_FixNameOverflow, 6)
 
 	return 0x4F6312;
 }
+
+// this is checked right before the TeamClass is instantiated -
+// it does not mean the AI will abandon this team if another team wants BuildLimit'ed units at the same time
+DEFINE_HOOK(50965E, HouseClass_CanInstantiateTeam, 5)
+{
+	GET(DWORD, ptrTask, EAX);
+	GET(DWORD, ptrOffset, ECX);
+
+	ptrTask += (ptrOffset - 4); // pointer math!
+	TaskForceEntryStruct * ptrEntry = reinterpret_cast<TaskForceEntryStruct *>(ptrTask); // evil! but works, don't ask me why
+
+	GET(HouseClass *, Owner, EBP);
+	enum { BuildLimitAllows = 0x5096BD, Absolutely = 0x509671, NoWay = 0x5096F1} CanBuild = NoWay;
+	if(TechnoTypeClass * Type = ptrEntry->Type) {
+		if(Type->GetFactoryType(true, true, false, Owner)) {
+			if(Ares::GlobalControls::AllowBypassBuildLimit[Owner->AIDifficulty]) {
+				CanBuild = BuildLimitAllows;
+			} else {
+				int remainLimit = HouseExt::BuildLimitRemaining(Owner, Type);
+				if(remainLimit >= ptrEntry->Amount) {
+					CanBuild = BuildLimitAllows;
+				} else {
+					CanBuild = NoWay;
+				}
+			}
+		} else {
+			CanBuild = BuildLimitAllows;
+		}
+	}
+	return CanBuild;
+}
+
