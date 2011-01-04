979bd0b775781692d5538ecabebd8725b0465526
DCoder <dcoder1337@gmail.com>
2011-01-04 19:16:48 +0000

    <strong>Related to issue #1037</strong> - I have not tested this, but projectile anim palettes should work now. Related to issue 1037 .
    git-svn-id: svn://svn.renegadeprojects.com/ares/trunk@941 859b54a9-7a54-0410-aeb3-f8d2f1fa40fd

--- src/Ext/AnimType/Hooks.cpp
+++ src/Ext/AnimType/Hooks.cpp
@@ -1,5 +1,6 @@
 #include "Body.h"
 #include <AnimClass.h>
+#include <BulletClass.h>
 
 DEFINE_HOOK(423122, AnimClass_Draw_SetPalette, 6)
 {
@@ -13,5 +14,20 @@ DEFINE_HOOK(423122, AnimClass_Draw_SetPalette, 6)
 		return 0x423358;
 	}
 
+	return 0;
+}
+
+
+DEFINE_HOOK(468379, BulletClass_Draw_SetAnimPalette, 6)
+{
+	GET(BulletClass *, Bullet, ESI);
+	if(AnimTypeClass * AnimType = AnimTypeClass::Find(Bullet->Type->ImageFile)) {
+		auto pData = AnimTypeExt::ExtMap.Find(AnimType);
+
+		if(pData->Palette.Convert) {
+			R->EBX<ConvertClass *>(pData->Palette.Convert);
+			return 0x4683D7;
+		}
+	}
 	return 0;
 }
