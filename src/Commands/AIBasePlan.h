b9e9ea3967475bc028e9afc876047ef9f8547b3d
DCoder <dcoder1337@gmail.com>
2011-03-05 12:48:55 +0000

    Added a Keyboard Command to report AI Base Plans, per Graion's request. Related to issue 1298 .
    git-svn-id: svn://svn.renegadeprojects.com/ares/trunk@995 859b54a9-7a54-0410-aeb3-f8d2f1fa40fd

---
+++ src/Commands/AIBasePlan.h
@@ -0,0 +1,58 @@
+#ifndef CMD_AIBASEPLAN_H
+#define CMD_AIBASEPLAN_H
+
+#include "Ares.h"
+#include "../Misc/Debug.h"
+
+#include "../Ext/House/Body.h"
+#include "../Ext/HouseType/Body.h"
+
+class AIBasePlanCommandClass : public CommandClass
+{
+public:
+	//Destructor
+	virtual ~AIBasePlanCommandClass(){}
+
+	//CommandClass
+	virtual const char* GetName()
+		{ return "Dump AI Base Plan"; }
+
+	virtual const wchar_t* GetUIName()
+		{ return L"AI Base Plan Logger"; }
+
+	virtual const wchar_t* GetUICategory()
+		{ return L"Development"; }
+
+	virtual const wchar_t* GetUIDescription()
+		{ return L"Dumps the AI Base Plans to the log"; }
+
+	virtual void Execute(DWORD dwUnk)
+	{
+		Debug::Log("AI Base Plans:\n");
+		for(int i = 0; i < HouseClass::Array->Count; ++i) {
+			auto H = HouseClass::Array->GetItem(i);
+			if(!H->IsHumanoid()) {
+				Debug::Log("#%02d: country %25s:\n", i, H->Type->ID);
+				auto b = &H->Base.BaseNodes;
+				for(int j = 0; j < b->Count; ++j) {
+					auto n = b->GetItem(j);
+					auto idx = n.BuildingTypeIndex;
+					if(idx >= 0) {
+						auto lbl = BuildingTypeClass::Array->GetItem(idx)->ID;
+						Debug::Log("\tNode #%03d: %s @ (%05d, %05d), Attempts so far: %d, Placed: %d\n"
+							, j, lbl, n.MapCoords.X, n.MapCoords.Y, n.Attempts, n.Placed);
+					} else {
+						Debug::Log("\tNode #%03d: Special %d @ (%05d, %05d), Attempts so far: %d, Placed: %d\n"
+							, j, idx, n.MapCoords.X, n.MapCoords.Y, n.Attempts, n.Placed);
+					}
+				}
+				Debug::Log("\n");
+			}
+		}
+	}
+
+	//Constructor
+	AIBasePlanCommandClass(){}
+};
+
+#endif
