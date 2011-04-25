e03ab75d7594ecdca0f3fbce5a1799feafd53788
DCoder <dcoder1337@gmail.com>
2011-04-25 10:42:27 +0000

    <strong>Related to issue #662</strong> - attempted to optimize gaps somewhat, performance improved noticably, but still unusable, so left the file as .cxx . If you want to test this, rename the file to .cpp and recompile.
    (What can I say, I was bored.)
    Related to issue 662 .
    
    git-svn-id: svn://svn.renegadeprojects.com/ares/trunk@1047 859b54a9-7a54-0410-aeb3-f8d2f1fa40fd

--- src/Ext/Techno/Hooks.Gap.cxx
+++ src/Ext/Techno/Hooks.Gap.cxx
@@ -1,3 +1,7 @@
+#include <Drawing.h>
+#include <YRDDraw.h>
+#include <MouseClass.h>
+
 #include "Body.h"
 #include "../TechnoType/Body.h"
 
@@ -92,10 +96,94 @@ DEFINE_HOOK(6FB723, TechnoClass_DeleteGap_RefreshMap, 5)
 
 DEFINE_HOOK(55AFB3, LogicClass_Update_Gaps, 6)
 {
-	if(TechnoExt::NeedsRegap) {
+	if(TechnoExt::NeedsRegap && !(Unsorted::CurrentFrame % 8)) {
 		TechnoExt::NeedsRegap = 0;
 		MapClass::Instance->sub_657CE0();
-		MapClass::Instance->sub_4F42F0(2);
+		MapClass::Instance->RedrawSidebar(2);
 	}
 	return 0;
+}
+
+static void StartTimer() {
+	reinterpret_cast<DSurface *>(MouseClass::Instance->unknown_121C)->Lock(0, 0);
+	reinterpret_cast<DSurface *>(MouseClass::Instance->unknown_1220)->Lock(0, 0);
+}
+
+static void EndTimer() {
+	reinterpret_cast<DSurface *>(MouseClass::Instance->unknown_1220)->Unlock();
+	reinterpret_cast<DSurface *>(MouseClass::Instance->unknown_121C)->Unlock();
+}
+
+DEFINE_HOOK(657CF2, MapClass_MinimapChanged_1S, 6)
+{
+	StartTimer();
+	return 0;
+}
+
+DEFINE_HOOK(657D35, MapClass_MinimapChanged_1E, 7)
+{
+	EndTimer();
+	return 0;
+}
+
+
+DEFINE_HOOK(657D3D, MapClass_MinimapChanged_2S, 6)
+{
+	StartTimer();
+	return 0;
+}
+
+DEFINE_HOOK(657D8A, MapClass_MinimapChanged_2E, 7)
+{
+	EndTimer();
+	return 0;
+}
+
+DEFINE_HOOK(655DFF, MapClass_UpdateMinimapPixel_1, 6)
+{
+	GET(TechnoTypeClass *, Type, EAX);
+	return (Type->GapGenerator)
+		? 0x655E66
+		: 0
+	;
+}
+
+DEFINE_HOOK(6561E4, MapClass_UpdateMinimapForCrates_2, 6)
+{
+	GET(TechnoClass *, Techno, ESI);
+	return (Techno->GetTechnoType()->GapGenerator)
+		? 0x656292
+		: 0
+	;
+}
+
+DEFINE_HOOK(6FB306, TechnoClass_CreateGap_TryOptimize, 6)
+{
+	GET(CellClass *, Cell, EAX);
+	const auto ShroudCounter = static_cast<signed int>(Cell->IsUnderShroud);
+	if(ShroudCounter >= 0 && ShroudCounter != 1) {
+		++Cell->IsUnderShroud;
+	}
+	++Cell->GapsCoveringThisCell;
+	if(ShroudCounter >= 1) {
+		Cell->CopyFlags &= ~(0x18);
+	}
+	return 0x6FB3BD;
+}
+
+DEFINE_HOOK(6FB5F0, TechnoClass_DeleteGap_TryOptimize, 6)
+{
+	GET(CellClass *, Cell, EAX);
+	--Cell->GapsCoveringThisCell;
+	const auto remainingGaps = static_cast<signed int>(Cell->GapsCoveringThisCell);
+	if(HouseClass::Player->SpySatActive) {
+		if(remainingGaps <= 0) {
+			--Cell->IsUnderShroud;
+			const auto ShroudCounter = static_cast<signed int>(Cell->IsUnderShroud);
+			if(ShroudCounter <= 0) {
+				Cell->CopyFlags |= 0x18;
+			}
+		}
+	}
+	return 0x6FB69E;
 }
