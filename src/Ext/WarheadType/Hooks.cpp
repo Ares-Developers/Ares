b1fdb691f3dfa1a5f9dbc0cba84c906fdc2fc7a6
DCoder1337 <dcoder1337@gmail.com>
2011-12-11 20:08:16 +0200

    Potential fix for issue 1517/bug 896130 (Temporal + Fire on the Move) : Funny westwood/Ares logic interaction was happening previously. Stock YR code creates a TemporalImUsing only for TechnoClass whose *Primary* is Temporal, not secondary or other. This means you couldn't have temporal secondaries. Ares fixed that by instantiating it for every unit that needs it. This unfortunately was done without properly investigating the IFV weapon switching code. This code replaces the vehicle's TemporalImUsing with the gunner's when it enters, and hands it back to the gunner when s/he exits the vehicle. After this, the unit's TIU is set to NULL. This means that 1) the unit's original TIU that Ares instantiated is leaked; 2) if the unit did have a Temporal weapon for its unmanned mode, it will crash when trying to fire it. In addition, this had an unexpected side-effect. UnitClass::GetFireError is called to check if a unit can fire at the moment, and that code says 'you cannot fire, you are moving' if a unit has a TemporalImUsing and is moving. This means that in Ares, an IFV cannot use MobileFire=yes (if it does, it will only be able to fire on the move correctly *after* a temporal-wielding gunner exits it). This commit should theoretically fix these problems: - Save the unit's original TIU before a gunner replaces it - Restore it after a gunner exits - just for completeness, sweep the saved pointer when running AnnounceInvalidPointer, even though it shouldn't come to this. - Change to 'you cannot fire, you are moving' to check if the unit is a gunner and currently wielding a temporal weapon OR not a gunner and in possession of a TIU. This condition *should* be correct...

--- src/Ext/WarheadType/Hooks.cpp
+++ src/Ext/WarheadType/Hooks.cpp
@@ -78,14 +78,14 @@ DEFINE_HOOK(46920B, BulletClass_Fire, 6) {
 
 // issue 472: deglob WarpAway
 DEFINE_HOOK(71A87B, TemporalClass_Update_CacheWH, 6) {
-	WarheadTypeExt::Temporal_WH = R->EAX<WeaponTypeClass *> ()->Warhead;
+	GET(WeaponTypeClass *, W, EAX);
+	WarheadTypeExt::Temporal_WH = W->Warhead;
 	return 0;
 }
 
 // issue 472: deglob WarpAway
 DEFINE_HOOK(71A900, TemporalClass_Update_WarpAway, 6) {
-	WarheadTypeExt::ExtData *pData = WarheadTypeExt::ExtMap.Find(WarheadTypeExt::Temporal_WH);
-
+	auto pData = WarheadTypeExt::ExtMap.Find(WarheadTypeExt::Temporal_WH);
 	R->EDX<AnimTypeClass *> (pData->Temporal_WarpAway);
 	return 0x71A906;
 }
