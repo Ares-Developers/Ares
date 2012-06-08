e22dd08b5d119fab8ccb88c677ff012faee61878
AlexB <Alexander-B@gmx.de>
2012-06-08 07:31:11 +0200

    The testing version warning on the game's loading screen will disappear if IS_RELEASE_VER is defined, without leaving a gap.

--- src/Misc/Status.cpp
+++ src/Misc/Status.cpp
@@ -1,6 +1,12 @@
 #include "../Ares.version.h"
 #include "Status.h"
 
+#ifdef IS_RELEASE_VER
+#define RELEASE 1
+#else
+#define RELEASE 0
+#endif
+
 Point2D StatusMessages::TLPoint = { 10, 340 };
 Point2D StatusMessages::Delta = { 0, 20 };
 
@@ -74,9 +80,13 @@ DEFINE_HOOK(687B21, Scenario_Start8, 5)
 
 DEFINE_HOOK(531413, Game_Start, 5)
 {
-	DSurface::Hidden->DrawText(L"Ares is active.", 10, 460/*500*/, COLOR_GREEN);
-	DSurface::Hidden->DrawText(L"This is a testing version, NOT a final product.", 20, 480, COLOR_RED);
-	DSurface::Hidden->DrawText(L"Bugs are to be expected.", 20, 500, COLOR_RED);
+	int topActive = RELEASE ? 500 : 460;
+
+	DSurface::Hidden->DrawText(L"Ares is active.", 10, topActive, COLOR_GREEN);
+	if(!RELEASE) {
+		DSurface::Hidden->DrawText(L"This is a testing version, NOT a final product.", 20, 480, COLOR_RED);
+		DSurface::Hidden->DrawText(L"Bugs are to be expected.", 20, 500, COLOR_RED);
+	}
 	DSurface::Hidden->DrawText(L"Ares is © pd, DCoder, Electro, Renegade and AlexB 2007 - 2012.", 10, 520, COLOR_GREEN);
 
 	wchar_t wVersion[256];
