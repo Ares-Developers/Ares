97ce113a8ce05bfbee4adaad1258f023ff5429b9
AlexB <Alexander-B@gmx.de>
2012-06-22 18:26:48 +0200

    Added the new tag [GlobalControls]DebugKeysEnabled= (boolean, defaults to yes). If it is set to no, the AI Base Plan, AI Control, Dump Types and Map Snapshot keyboard commands are disabled. If they are executed, the language string "TXT_COMMAND_DISABLED" is shown instead.
    This tag can be used to prevent users from gaining an unfair advantage in multi-player games, from revealing vital secrets, and from deliberately causing a Reconnection Error in release versions of a mod.
    
    TXT_COMMAND_DISABLED can include one "%s", which is replaced with the actual command name. In Ares, this string defaults to "The %s command is not available.".

--- src/Commands/Commands.h
+++ src/Commands/Commands.h
@@ -2,6 +2,30 @@
 #define ARES_COMMANDS_H
 
 #include <YRPP.h>
+#include <StringTable.h>
+
+class AresCommandClass : public CommandClass
+{
+protected:
+	/**
+		Adds a notification to the message list when debug key commands are disabled.
+
+		\return true, if debug commands are deactivated, false otherwise.
+		\author AlexB
+		\date 2012-06-21
+	*/
+	bool CheckDebugDeactivated() {
+		if(!Ares::GlobalControls::DebugKeysEnabled) {
+			if(const wchar_t* text = StringTable::LoadStringA("TXT_COMMAND_DISABLED", NULL, __LINE__, __FILE__)) {
+				wchar_t msg[0x100] = L"\0";
+				wsprintfW(msg, text, this->GetUIName());
+				MessageListClass::Instance->PrintMessage(msg);
+			}
+			return true;
+		}
+		return false;
+	}
+};
 
 class TestSomethingCommandClass : public CommandClass
 {
