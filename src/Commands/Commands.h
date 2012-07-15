f53af5200f171d7a2be200012cd53f8d4f514a2d
AlexB <Alexander-B@gmx.de>
2012-07-15 07:44:59 +0200

    Minor change: Removed some revealing macros which didn't help at all.

--- src/Commands/Commands.h
+++ src/Commands/Commands.h
@@ -16,7 +16,7 @@ protected:
 	*/
 	bool CheckDebugDeactivated() {
 		if(!Ares::GlobalControls::DebugKeysEnabled) {
-			if(const wchar_t* text = StringTable::LoadStringA("TXT_COMMAND_DISABLED", NULL, __LINE__, __FILE__)) {
+			if(const wchar_t* text = StringTable::LoadStringA("TXT_COMMAND_DISABLED")) {
 				wchar_t msg[0x100] = L"\0";
 				wsprintfW(msg, text, this->GetUIName());
 				MessageListClass::Instance->PrintMessage(msg);
