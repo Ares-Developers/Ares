02bedf39662e07a1fecfe2b9b5dbfd44899fce51
AlexB <Alexander-B@gmx.de>
2012-12-06 23:00:14 +0100

    Updated version number for 0.2p2.

--- src/Ares.version.h
+++ src/Ares.version.h
@@ -2,11 +2,11 @@
 #define VERSION_H
 
 // define this to switch to release version
-//#define IS_RELEASE_VER
+#define IS_RELEASE_VER
 
 #define VERSION_MAJOR 12
-#define VERSION_MINOR 339
-#define VERSION_REVISION 1154
+#define VERSION_MINOR 341
+#define VERSION_REVISION 1206
 
 #define SAVEGAME_MAGIC ((VERSION_MAJOR << 24) | (VERSION_MINOR << 16) | (VERSION_REVISION))
 
@@ -24,8 +24,8 @@
 
 #define PRODUCT_MAJOR 0
 #define PRODUCT_MINOR 2
-#define PRODUCT_REVISION 1
-#define PRODUCT_STR "0.2p1"
+#define PRODUCT_REVISION 2
+#define PRODUCT_STR "0.2p2"
 #define DISPLAY_STR PRODUCT_STR
 
 #else
