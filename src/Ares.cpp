8bcc01c6729d850ed981b037aa2f5563999b0572
AlexB <Alexander-B@gmx.de>
2013-06-04 04:36:04 +0200

    Implemented the Syringe handshake function.
    Currently, all 1.001 versions with known sizes (CD, TFD and UC) are accepted.
    Added some constants for known game versions.

--- src/Ares.cpp
+++ src/Ares.cpp
@@ -505,3 +505,84 @@ void Ares::UpdateStability() {
 			"and the original developers cannot be held responsible for any problems you might experience.");
 	}
 }
+
+#define YR_SIZE_1000 0x496110
+#define YR_SIZE_1001 0x497110
+#define YR_SIZE_1001_UC 0x497FE0
+#define YR_SIZE_NPATCH 0x5AB000
+
+#define YR_TIME_1000 0x3B846665
+#define YR_TIME_1001 0x3BDF544E
+
+#define YR_CRC_1000 0xB701D792
+#define YR_CRC_1001_CD 0x098465B3
+#define YR_CRC_1001_TFD 0xEB903080
+#define YR_CRC_1001_UC 0x1B499086
+
+SYRINGE_HANDSHAKE(pInfo)
+{
+	if(pInfo) {
+		const char* AcceptMsg = "Found Yuri's Revenge %s. Applying Ares " PRODUCT_STR ".";
+		const char* PatchDetectedMessage = "Found %s. Ares " PRODUCT_STR " is not compatible with other patches.";
+
+		const char* desc = nullptr;
+		const char* msg = nullptr;
+		bool allowed = false;
+
+		// accept tfd and cd version 1.001
+		if(pInfo->exeTimestamp == YR_TIME_1001) {
+
+			// don't accept expanded exes
+			switch(pInfo->exeFilesize) {
+			case YR_SIZE_1001:
+			case YR_SIZE_1001_UC:
+
+				// all versions allowed
+				switch(pInfo->exeCRC) {
+				case YR_CRC_1001_CD:
+					desc = "1.001 (CD)";
+					break;
+				case YR_CRC_1001_TFD:
+					desc = "1.001 (TFD)";
+					break;
+				case YR_CRC_1001_UC:
+					desc = "1.001 (UC)";
+					break;
+				default:
+					// no-cd, network port or other changes
+					desc = "1.001 (modified)";
+				}
+				msg = AcceptMsg;
+				allowed = true;
+				break;
+
+			case YR_SIZE_NPATCH:
+				// known patch size
+				desc = "RockPatch or an NPatch-derived patch";
+				msg = PatchDetectedMessage;
+				break;
+
+			default:
+				// expanded exe, unknown make
+				desc = "an unknown game patch";
+				msg = PatchDetectedMessage;
+			}
+		} else if(pInfo->exeTimestamp == YR_TIME_1000) {
+			// upgrade advice for version 1.000
+			desc = "1.000";
+			msg = "Found Yuri's Revenge 1.000 but Ares " PRODUCT_STR " requires version 1.001. Please update your copy of Yuri's Revenge first.";
+		} else {
+			// does not even compute...
+			msg = "Unknown executable. Ares " PRODUCT_STR " requires Command & Conquer Yuri's Revenge version 1.001 (gamemd.exe).";
+		}
+
+		// generate the output message
+		if(pInfo->Message) {
+			sprintf_s(pInfo->Message, pInfo->cchMessage, msg, desc);
+		}
+
+		return allowed ? S_OK : S_FALSE;
+	}
+
+	return E_POINTER;
+}
