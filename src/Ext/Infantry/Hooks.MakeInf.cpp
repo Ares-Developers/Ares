b0deb0309f4144c4486923bf95f4dd0b711ce641
AlexB <Alexander-B@gmx.de>
2014-06-04 00:17:43 +0200

    Related to #1320962: MakeInfantryOwner expanded.
    It should now work with InfantryExplode, FlamingInfantry, InfantryElectrocuted, InfantryNuked, InfantryVirus, InfantryMutate, and InfantryBrute.

--- src/Ext/Infantry/Hooks.MakeInf.cpp
+++ src/Ext/Infantry/Hooks.MakeInf.cpp
@@ -66,6 +66,90 @@ DEFINE_HOOK(51849A, InfantryClass_ReceiveDamage_DeathAnim, 5)
 	return 0x5184F2;
 }
 
+DEFINE_HOOK_AGAIN(518575, InfantryClass_ReceiveDamage_InfantryVirus1, 6)
+DEFINE_HOOK(5183DE, InfantryClass_ReceiveDamage_InfantryVirus1, 6)
+{
+	GET(InfantryClass*, pThis, ESI);
+	GET(AnimClass*, pAnim, EDI);
+	REF_STACK(args_ReceiveDamage, Arguments, STACK_OFFS(0xD0, -0x4));
+
+	// Rules->InfantryVirus animation has been created. set the owner and color.
+
+	auto pInvoker = Arguments.Attacker
+		? Arguments.Attacker->Owner
+		: Arguments.SourceHouse;
+
+	AnimTypeExt::SetMakeInfOwner(pAnim, pInvoker, pThis->Owner, pInvoker);
+
+	// bonus: don't require SpawnsParticle to be present
+
+	if(ParticleSystemClass::Array->ValidIndex(pAnim->Type->SpawnsParticle)) {
+		return 0;
+	}
+
+	return (R->get_Origin() == 0x5183DE) ? 0x518422 : 0x5185B9;
+}
+
+DEFINE_HOOK_AGAIN(518B93, InfantryClass_ReceiveDamage_Anims, 5) // InfantryBrute
+DEFINE_HOOK_AGAIN(518821, InfantryClass_ReceiveDamage_Anims, 5) // InfantryNuked
+DEFINE_HOOK_AGAIN(5187BB, InfantryClass_ReceiveDamage_Anims, 5) // InfantryHeadPop
+DEFINE_HOOK_AGAIN(518755, InfantryClass_ReceiveDamage_Anims, 5) // InfantryElectrocuted
+DEFINE_HOOK_AGAIN(5186F2, InfantryClass_ReceiveDamage_Anims, 5) // FlamingInfantry
+DEFINE_HOOK(518698, InfantryClass_ReceiveDamage_Anims, 5) // InfantryExplode
+{
+	GET(InfantryClass*, pThis, ESI);
+	GET(AnimClass*, pAnim, EAX);
+	REF_STACK(args_ReceiveDamage, Arguments, STACK_OFFS(0xD0, -0x4));
+
+	// animation has been created. set the owner and color.
+
+	auto pInvoker = Arguments.Attacker
+		? Arguments.Attacker->Owner
+		: Arguments.SourceHouse;
+
+	AnimTypeExt::SetMakeInfOwner(pAnim, nullptr, pThis->Owner, pInvoker);
+
+	return 0x5185F1;
+}
+
+DEFINE_HOOK(51887B, InfantryClass_ReceiveDamage_InfantryVirus2, A)
+{
+	GET(InfantryClass*, pThis, ESI);
+	GET(AnimClass*, pAnim, EAX);
+	REF_STACK(args_ReceiveDamage, Arguments, STACK_OFFS(0xD0, -0x4));
+
+	// Rules->InfantryVirus animation has been created. set the owner, but
+	// reset the color for default (invoker).
+
+	auto pInvoker = Arguments.Attacker
+		? Arguments.Attacker->Owner
+		: Arguments.SourceHouse;
+
+	auto res = AnimTypeExt::SetMakeInfOwner(pAnim, pInvoker, pThis->Owner, pInvoker);
+	if(res == AnimTypeExt::ExtData::MakeInfantryHouse::Invoker) {
+		pAnim->LightConvert = nullptr;
+	}
+
+	return 0x5185F1;
+}
+
+DEFINE_HOOK(518A96, InfantryClass_ReceiveDamage_InfantryMutate, 7)
+{
+	GET(InfantryClass*, pThis, ESI);
+	GET(AnimClass*, pAnim, EDI);
+	REF_STACK(args_ReceiveDamage, Arguments, STACK_OFFS(0xD0, -0x4));
+
+	// Rules->InfantryMutate animation has been created. set the owner and color.
+
+	auto pInvoker = Arguments.Attacker
+		? Arguments.Attacker->Owner
+		: Arguments.SourceHouse;
+
+	AnimTypeExt::SetMakeInfOwner(pAnim, pInvoker, pThis->Owner, pInvoker);
+
+	return 0x518AFF;
+}
+
 DEFINE_HOOK(6E232E, ActionClass_PlayAnimAt, 5)
 {
 	GET(TActionClass *, pAction, ESI);
@@ -83,7 +167,6 @@ DEFINE_HOOK(6E232E, ActionClass_PlayAnimAt, 5)
 
 	return 0x6E2368;
 }
-
 
 DEFINE_HOOK(469C4E, BulletClass_DetonateAt_DamageAnimSelected, 5)
 {
@@ -114,4 +197,3 @@ DEFINE_HOOK(469C4E, BulletClass_DetonateAt_DamageAnimSelected, 5)
 	R->EAX<AnimClass *>(Anim);
 	return 0x469C98;
 }
-
