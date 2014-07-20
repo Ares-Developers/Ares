7ef35bed41744326008711ce884ea28ab33d2e7a
AlexB <Alexander-B@gmx.de>
2014-07-20 20:18:39 +0200

    Scorch and Flamer tags reimplemented
    Tiberian Sun had Scorch and Flamer for animations, two tags that spawned fire animations defined by SmallFire and LargeFire. This has been reimplemented as it worked in Tiberian Sun, with the exception that it would not crash if SmallFire or LargeFire aren't defined, because some YR animations have Scorch=yes set while the animations are commented out.

--- src/Ext/AnimType/Hooks.cpp
+++ src/Ext/AnimType/Hooks.cpp
@@ -30,3 +30,64 @@ DEFINE_HOOK(468379, BulletClass_Draw_SetAnimPalette, 6)
 
 	return 0;
 }
+
+DEFINE_HOOK_AGAIN(42511B, AnimClass_Expired_ScorchFlamer, 7)
+DEFINE_HOOK_AGAIN(4250C9, AnimClass_Expired_ScorchFlamer, 7)
+DEFINE_HOOK(42513F, AnimClass_Expired_ScorchFlamer, 7)
+{
+	GET(AnimClass*, pThis, ESI);
+	auto pType = pThis->Type;
+
+	CoordStruct crd = pThis->GetCoords();
+
+	auto SpawnAnim = [&crd](AnimTypeClass* pType, int dist) {
+		if(!pType) {
+			return static_cast<AnimClass*>(nullptr);
+		}
+
+		CoordStruct crdAnim = crd;
+		if(dist > 0) {
+			auto crdNear = MapClass::GetRandomCoordsNear(crd, dist, false);
+			crdAnim = MapClass::PickInfantrySublocation(crdNear, true);
+		}
+
+		auto count = ScenarioClass::Instance->Random.RandomRanged(1, 2);
+		return GameCreate<AnimClass>(pType, crdAnim, 0, count, 0x600u, 0, false);
+	};
+
+	if(pType->Flamer) {
+		// always create at least one small fire
+		SpawnAnim(RulesClass::Instance->SmallFire, 64);
+
+		// 50% to create another small fire
+		if(ScenarioClass::Instance->Random.RandomRanged(0, 99) < 50) {
+			SpawnAnim(RulesClass::Instance->SmallFire, 160);
+		}
+
+		// 50% chance to create a large fire
+		if(ScenarioClass::Instance->Random.RandomRanged(0, 99) < 50) {
+			SpawnAnim(RulesClass::Instance->LargeFire, 112);
+		}
+
+	} else if(pType->Scorch) {
+		// creates a SmallFire anim that is attached to the same object
+		// this anim is attached to.
+		if(pThis->GetHeight() < 10) {
+			switch(pThis->GetCell()->LandType) {
+			case LandType::Water:
+			case LandType::Beach:
+			case LandType::Ice:
+			case LandType::Rock:
+				break;
+			default:
+				if(auto pAnim = SpawnAnim(RulesClass::Instance->SmallFire, 0)) {
+					if(pThis->OwnerObject) {
+						pAnim->SetOwnerObject(pThis->OwnerObject);
+					}
+				}
+			}
+		}
+	}
+
+	return 0;
+}
