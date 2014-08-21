7a664f3628b8ab3e6743555c8d99120af5eebc36
AlexB <Alexander-B@gmx.de>
2014-08-21 17:50:23 +0200

    Indentation removed.

--- src/Misc/SWTypes/Reveal.h
+++ src/Misc/SWTypes/Reveal.h
@@ -5,16 +5,16 @@
 
 class SW_Reveal : public NewSWType
 {
-	public:
-		SW_Reveal() : NewSWType()
-			{ };
+public:
+	SW_Reveal() : NewSWType()
+		{ };
 
-		virtual void Initialize(SWTypeExt::ExtData *pData, SuperWeaponTypeClass *pSW) override;
-		virtual bool Activate(SuperClass* pThis, const CellStruct &Coords, bool IsPlayer) override;
-		virtual bool HandlesType(int type) const override;
+	virtual void Initialize(SWTypeExt::ExtData *pData, SuperWeaponTypeClass *pSW) override;
+	virtual bool Activate(SuperClass* pThis, const CellStruct &Coords, bool IsPlayer) override;
+	virtual bool HandlesType(int type) const override;
 
-		virtual int GetSound(const SWTypeExt::ExtData* pData) const override;
-		virtual SWRange GetRange(const SWTypeExt::ExtData* pData) const override;
+	virtual int GetSound(const SWTypeExt::ExtData* pData) const override;
+	virtual SWRange GetRange(const SWTypeExt::ExtData* pData) const override;
 };
 
 #endif
