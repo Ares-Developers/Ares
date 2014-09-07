b2cc6107e16459dd04f346089ef1af5961159557
AlexB <Alexander-B@gmx.de>
2014-09-07 18:07:32 +0200

    Update for Rank enum class changes.

--- src/Ext/Techno/Hooks.Veterancy.cpp
+++ src/Ext/Techno/Hooks.Veterancy.cpp
@@ -162,7 +162,7 @@ DEFINE_HOOK(702E9D, TechnoClass_RegisterDestruction_Veterancy, 6) {
 			// gunners need to be promoted manually, or they won't only get
 			// the experience until after they exited their transport once.
 			if(promoteImmediately) {
-				Rank::Value newRank = pExperience->Veterancy.GetRemainingLevel();
+				auto newRank = pExperience->Veterancy.GetRemainingLevel();
 
 				if(pExperience->CurrentRanking != newRank) {
 					if(pExperience->CurrentRanking != Rank::Invalid) {
