03dc66f7c9e0c539c5b493702917e816dfaa6019
AlexB <Alexander-B@gmx.de>
2014-12-29 08:38:49 +0100

    Formatting

--- src/Misc/PoweredUnitClass.h
+++ src/Misc/PoweredUnitClass.h
@@ -8,23 +8,23 @@ class PoweredUnitClass
 {
 private:
 	static const int ScanInterval = 15;		//!< Minimum delay between scans in frames.
-	
+
 	TechnoClass* Techno;
 	int LastScan;							//!< Frame number when the last scan was performed.
 	bool Powered;							//!< Whether the unit has a building providing power. NOT the same as being online.
-	
+
 	bool IsPoweredBy(HouseClass* Owner) const;
 	void PowerUp();
 	bool PowerDown();
 public:
 	PoweredUnitClass(TechnoClass* Techno) : Techno(Techno), LastScan(0), Powered(true)
 	{ }
-	
+
 	~PoweredUnitClass() = default;
 
 	//!< Updates this Powered Unit's status. Returns whether the unit should stay alive.
 	bool Update();
-	
+
 	//!< Whether the unit has a building providing power. NOT the same as being online.
 	inline bool IsPowered() const {
 		return this->Powered;
