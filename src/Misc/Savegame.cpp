b2bf3f75d4ce854c0d835333339b34dcaab0ece7
AlexB <Alexander-B@gmx.de>
2014-12-30 04:06:10 +0100

    Ported over the hooks calling the global save and load methods from dc03-savegames, with changes.
    Fixed broken hooks that were placed where they couldn't resume easily. Used a static array of bytes as a fake function end to jump back to.

--- src/Misc/Savegame.cpp
+++ src/Misc/Savegame.cpp
@@ -1,1 +1,49 @@
-#include "Savegame.h"
+#include "SavegameDef.h"
+
+#include "../Ares.h"
+
+DEFINE_HOOK(67D300, SaveGame_Start, 5)
+{
+	Ares::SaveGame();
+	return 0;
+}
+
+const byte SaveGame_ReturnCode[] = {
+	0x85, 0xC0,       // test eax, eax
+	0x5F,             // pop edi
+	0x5E,             // pop esi
+	0x5D,             // pop ebp
+	0x5B,             // pop ebx
+	0x0F, 0x9D, 0xC0, // setnl al
+	0x83, 0xC4, 0x08, // add esp, 8
+	0xC3              // retn
+};
+
+DEFINE_HOOK(67E42E, SaveGame, 5)
+{
+	GET(HRESULT, Status, EAX);
+
+	if(SUCCEEDED(Status)) {
+		GET(IStream *, pStm, ESI);
+
+		Status = Ares::SaveGameData(pStm);
+		R->EAX<HRESULT>(Status);
+	}
+
+	return reinterpret_cast<DWORD>(SaveGame_ReturnCode);
+}
+
+DEFINE_HOOK(67E730, LoadGame_Start, 5)
+{
+	Ares::LoadGame();
+	return 0;
+}
+
+DEFINE_HOOK(67F7C8, LoadGame_End, 5)
+{
+	GET(IStream *, pStm, ESI);
+
+	Ares::LoadGameData(pStm);
+
+	return 0;
+}
