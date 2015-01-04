Ares-Developers/Ares

commit 51977ff8c617308010bf90bf1220423087ae9e2e

Update for Action enum class changes.
- Converted #defines to static const Actions
- LastAction converted to Action (and finally initialized)

src/Misc/Actions.cpp
@@ -130,9 +130,9 @@ DEFINE_HOOK(5BDE64, Actions_AnimateCursor2, 6)
 DEFINE_HOOK(4D7524, Actions_AllowForFootClass, 9)
 {
 	//overwrote the ja, need to replicate it
-	unsigned int CursorIndex = R->EAX();
-	if(CursorIndex > 0x46) {
-		if(CursorIndex == (SW_YES_CURSOR - 1) || CursorIndex == (SW_NO_CURSOR - 1)) {
+	auto CursorIndex = static_cast<Action>(R->EAX() - 1);
+	if(CursorIndex > Action::ForceShield) {
+		if(CursorIndex == Actions::SuperWeaponAllowed || CursorIndex == Actions::SuperWeaponDisallowed) {
 			return 0x4D769F;
 		} else {
 			return 0x4D7CC0;
@@ -267,10 +267,10 @@ DEFINE_HOOK(6929FC, DisplayClass_ChooseAction_CanSell, 7)
 	switch(Target->WhatAmI()) {
 		case abs_Aircraft:
 		case abs_Unit:
-			R->Stack<DWORD>(0x10, act_SellUnit);
+			R->Stack(0x10, Action::SellUnit);
 			return 0x692B06;
 		case abs_Building:
-			R->Stack<DWORD>(0x10, Target->IsStrange() ? act_NoSell : act_Sell);
+			R->Stack(0x10, Target->IsStrange() ? Action::NoSell : Action::Sell);
 			return 0x692B06;
 		default:
 			return 0x692AFE;

src/Ext/Building/Hooks.Trenches.cpp
@@ -104,7 +104,7 @@ DEFINE_HOOK(44725F, BuildingClass_GetCursorOverObject_TargetABuilding, 5)
 
 		if(curBuildExt->canTraverseTo(targetBuilding)) {
 			//show entry cursor, hooked up to traversal logic in Misc/Network.cpp -> AresNetEvent::Handlers::RespondToTrenchRedirectClick
-			R->EAX<eAction>(act_Enter);
+			R->EAX(Action::Enter);
 			return 0x447273;
 		}
 	}
@@ -114,7 +114,7 @@ DEFINE_HOOK(44725F, BuildingClass_GetCursorOverObject_TargetABuilding, 5)
 
 DEFINE_HOOK(443414, BuildingClass_ClickedAction, 6)
 {
-	GET(eAction, Action, EAX);
+	GET(::Action, Action, EAX);
 	GET(BuildingClass *, pThis, ECX);
 
 	GET_STACK(ObjectClass *, pTarget, 0x8);
@@ -128,7 +128,7 @@ DEFINE_HOOK(443414, BuildingClass_ClickedAction, 6)
 	}
 
 	// trenches
-	if(Action == act_Enter) {
+	if(Action == Action::Enter) {
 		if(BuildingClass *pTargetBuilding = specific_cast<BuildingClass *>(pTarget)) {
 			CoordStruct XYZ = pTargetBuilding->GetCoords();
 			CellStruct tgt = CellClass::Coord2Cell(XYZ);

src/Ext/Infantry/Body.cpp
@@ -38,7 +38,7 @@ bool InfantryExt::ExtData::IsOccupant() {
 	\author AlexB
 	\date 2012-07-22
 */
-eAction InfantryExt::GetEngineerEnterEnemyBuildingAction(BuildingClass *pBld) {
+Action InfantryExt::GetEngineerEnterEnemyBuildingAction(BuildingClass *pBld) {
 	// no other mode than skirmish allows to disable it, so we only check whether
 	// multi engineer is disabled there. for all other modes, it's always on.
 	// single player campaigns also use special multi engineer behavior.
@@ -65,13 +65,13 @@ eAction InfantryExt::GetEngineerEnterEnemyBuildingAction(BuildingClass *pBld) {
 		if(!RulesExt::Global()->EngineerAlwaysCaptureTech || !isTech) {
 			// no civil structure. apply new logic.
 			if(pBld->GetHealthPercentage() > RulesClass::Global()->EngineerCaptureLevel) {
-				return (RulesExt::Global()->EngineerDamage > 0) ? act_Damage : act_NoEnter;
+				return (RulesExt::Global()->EngineerDamage > 0) ? Action::Damage : Action::NoEnter;
 			}
 		}
 	}
 
 	// default.
-	return act_Capture;
+	return Action::Capture;
 }
 
 // =============================

src/Ext/Infantry/Body.h
@@ -33,7 +33,7 @@ public:
 
 	static Container<InfantryExt> ExtMap;
 
-	static eAction GetEngineerEnterEnemyBuildingAction(BuildingClass *pBld);
+	static Action GetEngineerEnterEnemyBuildingAction(BuildingClass *pBld);
 };
 
 #endif

src/Ext/Infantry/Hooks.cpp
@@ -115,10 +115,10 @@ DEFINE_HOOK(51E5BB, InfantryClass_GetCursorOverObject_MultiEngineerA, 7) {
 
 DEFINE_HOOK(51E5E1, InfantryClass_GetCursorOverObject_MultiEngineerB, 7) {
 	GET(BuildingClass *, pBld, ECX);
-	eAction ret = InfantryExt::GetEngineerEnterEnemyBuildingAction(pBld);
+	Action ret = InfantryExt::GetEngineerEnterEnemyBuildingAction(pBld);
 
 	// use a dedicated cursor
-	if(ret == act_Damage) {
+	if(ret == Action::Damage) {
 		Actions::Set(&RulesExt::Global()->EngineerDamageCursor);
 	}
 
@@ -132,8 +132,8 @@ DEFINE_HOOK(519D9C, InfantryClass_UpdatePosition_MultiEngineer, 5) {
 	GET(BuildingClass *, pBld, EDI);
 
 	// damage or capture
-	eAction action = InfantryExt::GetEngineerEnterEnemyBuildingAction(pBld);
-	if(action == act_Damage) {
+	Action action = InfantryExt::GetEngineerEnterEnemyBuildingAction(pBld);
+	if(action == Action::Damage) {
 		int Damage = static_cast<int>(ceil(pBld->Type->Strength * RulesExt::Global()->EngineerDamage));
 		pBld->ReceiveDamage(&Damage, 0, RulesClass::Global()->C4Warhead, pEngi, true, false, nullptr);
 		return 0x51A010;
@@ -186,7 +186,7 @@ DEFINE_HOOK(51EB48, InfantryClass_GetCursorOverObject_IvanGrinder, A)
 			if(!InputManagerClass::Instance->IsForceFireKeyPressed()) {
 				static byte return_grind[] = {
 					0x5F, 0x5E, 0x5D, // pop edi, esi and ebp
-					0xB8, act_Repair, 0x00, 0x00, 0x00, // eax = act_Repair (not act_Eaten)
+					0xB8, 0x0B, 0x00, 0x00, 0x00, // eax = Action::Repair (not Action::Eaten)
 					0x5B, 0x83, 0xC4, 0x28, // esp += 0x28
 					0xC2, 0x08, 0x00 // retn 8
 				};

src/Ext/SWType/Body.cpp
@@ -75,7 +75,7 @@ void SWTypeExt::ExtData::LoadFromRulesFile(SuperWeaponTypeClass *pThis, CCINICla
 	INI_EX exINI(pINI);
 
 	if(exINI.ReadString(section, "Action") && !_strcmpi(exINI.value(), "Custom")) {
-		pThis->Action = SW_YES_CURSOR;
+		pThis->Action = Actions::SuperWeaponAllowed;
 	}
 
 	if(exINI.ReadString(section, "Type")) {
@@ -92,7 +92,7 @@ void SWTypeExt::ExtData::LoadFromRulesFile(SuperWeaponTypeClass *pThis, CCINICla
 
 	// if this is handled by a NewSWType, initialize it.
 	if(auto pNewSWType = this->GetNewSWType()) {
-		pThis->Action = SW_YES_CURSOR;
+		pThis->Action = Actions::SuperWeaponAllowed;
 		pNewSWType->Initialize(this, pThis);
 	}
 	this->LastAction = pThis->Action;

src/Ext/SWType/Body.h
@@ -245,7 +245,7 @@ public:
 		AresPCXFile SidebarPCX;
 
 		SuperWeaponType HandledByNewSWType;
-		int LastAction;
+		Action LastAction;
 
 		ExtData(TT* const OwnerObject) : Extension<TT>(OwnerObject),
 			SpyPlane_TypeIndex (0),
@@ -310,6 +310,7 @@ public:
 			SW_Designators (),
 			SW_AnyDesignator (false),
 			HandledByNewSWType (SuperWeaponType::Invalid),
+			LastAction (Action::None),
 			CameoPal(),
 			SW_DeliverBuildups (false)
 			{

src/Ext/SWType/Hooks.cpp
@@ -13,10 +13,10 @@ DEFINE_HOOK(6CEF84, SuperWeaponTypeClass_GetCursorOverObject, 7)
 	SWTypeExt::ExtData *pData = SWTypeExt::ExtMap.Find(pThis);
 	auto pType = pData->GetNewSWType();
 
-	if((pThis->Action == SW_YES_CURSOR) || pType) {
+	if((pThis->Action == Actions::SuperWeaponAllowed) || pType) {
 		GET_STACK(CellStruct *, pMapCoords, 0x0C);
 
-		int Action = SW_YES_CURSOR;
+		auto Action = Actions::SuperWeaponAllowed;
 
 		// prevent firing into shroud
 		if(!pData->SW_FireToShroud) {
@@ -24,20 +24,20 @@ DEFINE_HOOK(6CEF84, SuperWeaponTypeClass_GetCursorOverObject, 7)
 			CoordStruct Crd = pCell->GetCoords();
 
 			if(MapClass::Instance->IsLocationShrouded(Crd)) {
-				Action = SW_NO_CURSOR;
+				Action = Actions::SuperWeaponDisallowed;
 			}
 		}
 
 		// new SW types have to check whether the coordinates are valid.
-		if(Action == SW_YES_CURSOR) {
+		if(Action == Actions::SuperWeaponAllowed) {
 			if(pType && !pType->CanFireAt(pData, HouseClass::Player, *pMapCoords)) {
-				Action = SW_NO_CURSOR;
+				Action = Actions::SuperWeaponDisallowed;
 			}
 		}
 
 		R->EAX(Action);
 
-		if(Action == SW_YES_CURSOR) {
+		if(Action == Actions::SuperWeaponAllowed) {
 			SWTypeExt::CurrentSWType = pThis;
 			Actions::Set(&pData->SW_Cursor, pData->SW_FireToShroud);
 		} else {
@@ -65,10 +65,10 @@ DEFINE_HOOK(653B3A, RadarClass_GetMouseAction_CustomSWAction, 5)
 		SWTypeExt::ExtData *pData = SWTypeExt::ExtMap.Find(pThis);
 		auto pType = pData->GetNewSWType();
 
-		if((pThis->Action == SW_YES_CURSOR) || pType) {
+		if((pThis->Action == Actions::SuperWeaponAllowed) || pType) {
 			GET_STACK(CellStruct, MapCoords, STACK_OFFS(0x54, 0x3C));
 
-			int Action = SW_YES_CURSOR;
+			auto Action = Actions::SuperWeaponAllowed;
 
 			// prevent firing into shroud
 			if(!pData->SW_FireToShroud) {
@@ -76,20 +76,20 @@ DEFINE_HOOK(653B3A, RadarClass_GetMouseAction_CustomSWAction, 5)
 				CoordStruct Crd = pCell->GetCoords();
 
 				if(MapClass::Instance->IsLocationShrouded(Crd)) {
-					Action = SW_NO_CURSOR;
+					Action = Actions::SuperWeaponDisallowed;
 				}
 			}
 
 			// new SW types have to check whether the coordinates are valid.
-			if(Action == SW_YES_CURSOR) {
+			if(Action == Actions::SuperWeaponAllowed) {
 				if(pType && !pType->CanFireAt(pData, HouseClass::Player, MapCoords)) {
-					Action = SW_NO_CURSOR;
+					Action = Actions::SuperWeaponDisallowed;
 				}
 			}
 
 			R->ESI(Action);
 
-			if(Action == SW_YES_CURSOR) {
+			if(Action == Actions::SuperWeaponAllowed) {
 				SWTypeExt::CurrentSWType = pThis;
 				Actions::Set(&pData->SW_Cursor, pData->SW_FireToShroud);
 			} else {
@@ -206,8 +206,8 @@ DEFINE_HOOK(6CEE96, SuperWeaponTypeClass_GetTypeIndex, 5)
 // translates SW click to type
 DEFINE_HOOK(4AC20C, DisplayClass_LMBUp, 7)
 {
-	int Action = R->Stack32(0x9C);
-	if(Action < SW_NO_CURSOR) {
+	auto Action = static_cast<::Action>(R->Stack32(0x9C));
+	if(Action < Actions::SuperWeaponDisallowed) {
 		// get the actual firing SW type instead of just the first type of the
 		// requested action. this allows clones to work for legacy SWs (the new
 		// ones use SW_*_CURSORs). we have to check that the action matches the
@@ -222,7 +222,7 @@ DEFINE_HOOK(4AC20C, DisplayClass_LMBUp, 7)
 		R->EAX(pSW);
 		return pSW ? 0x4AC21C : 0x4AC294;
 	}
-	else if(Action == SW_NO_CURSOR) {
+	else if(Action == Actions::SuperWeaponDisallowed) {
 		R->EAX(0);
 		return 0x4AC294;
 	}
@@ -806,7 +806,7 @@ DEFINE_HOOK(6CBD6B, SuperClass_Update_DrainMoney, 8) {
 
 // used only to find the nuke for ICBM crates. only supports nukes fully.
 DEFINE_HOOK(6CEEB0, SuperWeaponTypeClass_FindFirstOfAction, 8) {
-	GET(int, action, ECX);
+	GET(Action, action, ECX);
 
 	R->EAX(0);
 

commit 3e25a61e04be323b9560e8fb175f45ab6b0da3e8

Related to #1407367: Crush command not working any more
This fixes a regression introduced in 51977ff8c617308010bf90bf1220423087ae9e2e.

The wrong code subtracted 1 from an decremented index again, rendering Move an invalid action. To get the original value, "+ 1" would have been correct, but would also made 0 (None) a valid value. Now, None and all values above Airstrike are invalid, except for the new Ares super weapon ones.

src/Ares.version.h
@@ -4,9 +4,9 @@
 // define this to switch to release version
 //#define IS_RELEASE_VER
 
-#define VERSION_MAJOR 14
-#define VERSION_MINOR 364
-#define VERSION_REVISION 1321
+#define VERSION_MAJOR 15
+#define VERSION_MINOR 4
+#define VERSION_REVISION 146
 
 #define SAVEGAME_MAGIC ((VERSION_MAJOR << 24) | (VERSION_MINOR << 12) | (VERSION_REVISION))
 

src/Misc/Actions.cpp
@@ -130,8 +130,8 @@ DEFINE_HOOK(5BDE64, Actions_AnimateCursor2, 6)
 DEFINE_HOOK(4D7524, Actions_AllowForFootClass, 9)
 {
 	//overwrote the ja, need to replicate it
-	auto CursorIndex = static_cast<Action>(R->EAX() - 1);
-	if(CursorIndex > Action::ForceShield) {
+	GET(Action, CursorIndex, EBP);
+	if(CursorIndex == Action::None || CursorIndex > Action::Airstrike) {
 		if(CursorIndex == Actions::SuperWeaponAllowed || CursorIndex == Actions::SuperWeaponDisallowed) {
 			return 0x4D769F;
 		} else {