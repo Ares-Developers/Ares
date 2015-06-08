045d106743e763ab310cde1ecbdbc70f7f1bb547
AlexB <Alexander-B@gmx.de>
2015-06-08 03:55:41 +0200

    Refactored the Splits and Airburst code.
    While this looks a bit nicer, it's still a lot of unnecessary calculations. Only kept for reference.

--- src/Ext/Bullet/Hooks.cpp
+++ src/Ext/Bullet/Hooks.cpp
@@ -193,7 +193,7 @@ DEFINE_HOOK(469EBA, BulletClass_DetonateAt_Splits, 6)
 
 				// firer would hit itself
 				if(pTarget == pThis->Owner) {
-					if(random.RandomDouble() > 0.5f) {
+					if(random.RandomDouble() > 0.5) {
 						index = random.RandomRanged(0, targets.Count - 1);
 						pTarget = targets.GetItem(index);
 					}
@@ -209,21 +209,20 @@ DEFINE_HOOK(469EBA, BulletClass_DetonateAt_Splits, 6)
 				auto pSplitExt = BulletTypeExt::ExtMap.Find(pWeapon->Projectile);
 
 				if(auto pBullet = pSplitExt->CreateBullet(pTarget, pThis->Owner, pWeapon)) {
-					const double doubleA = -0.00009587672516830327;
-					const double doubleB = 4.712436918747274;
+					DirStruct const dir(5, static_cast<short>(random.RandomRanged(0, 31)));
+					auto const radians = dir.radians();
 
-					int a = (random.RandomRanged(0, 32) << 8) - 16383;
-					double b = a * doubleA;
-					double sin_b = Math::sin(b);
-					double cos_b = Math::cos(b);
+					double const sin_rad = Math::sin(radians);
+					double const cos_rad = Math::cos(radians);
 
-					double sin_c = Math::sin(doubleB);
-					double cos_c = Math::cos(doubleB);
+					auto const threeHalfPi = 1.5 * Math::Pi;
+					auto const sin_factor = Math::sin(threeHalfPi); // -1.0
+					auto const cos_factor = Math::cos(threeHalfPi); // 0.0
 
 					BulletVelocity velocity;
-					velocity.X = cos_c * cos_b * pBullet->Speed;
-					velocity.Y = cos_c * sin_b * pBullet->Speed;
-					velocity.Z = sin_c * pBullet->Speed;
+					velocity.X = cos_factor * cos_rad * pBullet->Speed; // 0.0 * ...
+					velocity.Y = cos_factor * sin_rad * pBullet->Speed; // 0.0 * ...
+					velocity.Z = sin_factor * pBullet->Speed; // -1.0 * ...
 
 					pBullet->MoveTo(pThis->Location, velocity);
 				}
