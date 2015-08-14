c56a2b317a3a84e80d0a50c26362783df51bc676
AlexB <Alexander-B@gmx.de>
2015-08-14 17:06:06 +0200

    Minor changes

--- src/Ares.UISettings.cpp
+++ src/Ares.UISettings.cpp
@@ -67,9 +67,9 @@ void Ares::UISettings::Load(CCINIClass *pINI) {
 		return;
 	}
 
-	const char* section = "UISettings";
+	auto const section = "UISettings";
 
-	auto ReadUIAction = [pINI, section](const char* name, Ares::UISettings::UIAction &value) {
+	auto const ReadUIAction = [pINI, section](const char* name, Ares::UISettings::UIAction &value) {
 		if(pINI->ReadString(section, name, "default", Ares::readBuffer)) {
 			value = Interface::parseUIAction(Ares::readBuffer, value);
 		}
@@ -93,106 +93,106 @@ void Ares::UISettings::Load(CCINIClass *pINI) {
 	CampaignListSize = pINI->ReadInteger(section, "CampaignListSize", CampaignListSize);
 
 	// read the campaigns that can be started from the default campaign selection menu
-	auto ReadCampaign = [pINI, section](const char* name, CampaignData *value,
+	auto const ReadCampaign = [pINI, section](const char* name, CampaignData& value,
 		const char* defBattle, const char* defImage, const char* defPalette,
 		const char* defSubline)
 	{
 		if(pINI->ReadString(section, name, defBattle, Ares::readBuffer)) {
-			AresCRT::strCopy(value->Battle, Ares::readBuffer);
+			AresCRT::strCopy(value.Battle, Ares::readBuffer);
 		}
 
 		// load the image
 		char buffer[0x20];
 		sprintf_s(buffer, 0x20, "%s.Image", name);
 		if(pINI->ReadString(section, buffer, defImage, Ares::readBuffer)) {
-			value->Image = FileSystem::LoadSHPFile(Ares::readBuffer);
+			value.Image = FileSystem::LoadSHPFile(Ares::readBuffer);
 		}
 
 		// read the palette
 		sprintf_s(buffer, 0x20, "%s.Palette", name);
-		if(value->Palette) {
-			delete value->Palette;
+		if(value.Palette) {
+			delete value.Palette;
 		}
-		value->Palette = new CustomPalette();
-		value->Palette->LoadFromINI(pINI, section, buffer, defPalette);
+		value.Palette = new CustomPalette();
+		value.Palette->LoadFromINI(pINI, section, buffer, defPalette);
 
 		// get the subline and tooltip
 		sprintf_s(buffer, 0x20, "%s.Subline", name);
 		if(pINI->ReadString(section, buffer, defSubline, Ares::readBuffer)) {
-			AresCRT::strCopy(value->Subline, Ares::readBuffer);
+			AresCRT::strCopy(value.Subline, Ares::readBuffer);
 		}
 
 		sprintf_s(buffer, 0x20, "%s.Tooltip", name);
-		if(pINI->ReadString(section, buffer, value->Subline, Ares::readBuffer)) {
-			AresCRT::strCopy(value->ToolTip, Ares::readBuffer);
+		if(pINI->ReadString(section, buffer, value.Subline, Ares::readBuffer)) {
+			AresCRT::strCopy(value.ToolTip, Ares::readBuffer);
 		}
 
 		// is this a valid campaign?
-		value->Valid = (*value->Battle != 0) && value->Image && _strcmpi(value->Battle, "no");
+		value.Valid = (value.Battle[0] != 0) && value.Image && _strcmpi(value.Battle, "no");
 	};
 
-	ReadCampaign("Campaign1", &Campaigns[0], "ALL1", "FSALG.SHP", "FSALG.PAL", "STT:AlliedCampaignIcon");
-	ReadCampaign("Campaign2", &Campaigns[1], "SOV1", "FSSLG.SHP", "FSSLG.PAL", "STT:SovietCampaignIcon");
-	ReadCampaign("Campaign3", &Campaigns[2], "TUT1", "", "FSBCLG.PAL", "STT:CampaignAnimTutorial");
-	ReadCampaign("Campaign4", &Campaigns[3], "", "", "", "");
+	ReadCampaign("Campaign1", Campaigns[0], "ALL1", "FSALG.SHP", "FSALG.PAL", "STT:AlliedCampaignIcon");
+	ReadCampaign("Campaign2", Campaigns[1], "SOV1", "FSSLG.SHP", "FSSLG.PAL", "STT:SovietCampaignIcon");
+	ReadCampaign("Campaign3", Campaigns[2], "TUT1", "", "FSBCLG.PAL", "STT:CampaignAnimTutorial");
+	ReadCampaign("Campaign4", Campaigns[3], "", "", "", "");
 
 	// color settings. these are the selectable colors used in the user interface
 	// and the mapping to link to the corresponding in-game color schemes.
-	const char* section2 = "Colors";
+	auto const section2 = "Colors";
 
-	auto ParseColorInt = [pINI](const char* section, const char* key, int defColor) -> int {
+	auto const ParseColorInt = [pINI](const char* section, const char* key, int defColor) -> int {
 		ColorStruct default(defColor & 0xFF, (defColor >> 8) & 0xFF, (defColor >> 16) & 0xFF);
 		auto const color = pINI->ReadColor(section, key, default);
 		return color.R | color.G << 8 | color.B << 16;
 	};
 
-	auto ReadColor = [pINI, section2, ParseColorInt](const char* name, ColorData *value, int colorRGB, const char* defTooltip, const char* defColorScheme) {
+	auto const ReadColor = [pINI, section2, ParseColorInt](const char* name, ColorData& value, int colorRGB, const char* defTooltip, const char* defColorScheme) {
 		// load the tooltip string
 		char buffer[0x20];
 		sprintf_s(buffer, 0x20, "%s.Tooltip", name);
 		if(pINI->ReadString(section2, buffer, defTooltip, Ares::readBuffer)) {
-			value->sttToolTipSublineText = StringTable::LoadStringA(Ares::readBuffer);
+			value.sttToolTipSublineText = StringTable::LoadString(Ares::readBuffer);
 		}
 
 		sprintf_s(buffer, 0x20, "%s.ColorScheme", name);
 		if(pINI->ReadString(section2, buffer, defColorScheme, Ares::readBuffer)) {
-			AresCRT::strCopy(value->colorScheme, Ares::readBuffer);
+			AresCRT::strCopy(value.colorScheme, Ares::readBuffer);
 		}
 
 		sprintf_s(buffer, 0x20, "%s.DisplayColor", name);
-		value->colorRGB = ParseColorInt(section2, buffer, colorRGB);
+		value.colorRGB = ParseColorInt(section2, buffer, colorRGB);
 
-		value->colorSchemeIndex = -1;
-		value->selectedIndex = -1;
+		value.colorSchemeIndex = -1;
+		value.selectedIndex = -1;
 	};
 
-	int* defColors = reinterpret_cast<int*>(0x8316A8);
-	ColorCount = std::max(std::min(pINI->ReadInteger(section2, "Count", ColorCount), MaxColorCount), 8);
+	ColorCount = Math::clamp(pINI->ReadInteger(section2, "Count", ColorCount), 8, MaxColorCount);
 
 	// original color schemes
-	ReadColor("Observer", &Colors[0], defColors[8], "STT:PlayerColorObserver", "LightGrey");
-	ReadColor("Slot1", &Colors[1], defColors[0], "STT:PlayerColorGold", "Gold");
-	ReadColor("Slot2", &Colors[2], defColors[1], "STT:PlayerColorRed", "DarkRed");
-	ReadColor("Slot3", &Colors[3], defColors[2], "STT:PlayerColorBlue", "DarkBlue");
-	ReadColor("Slot4", &Colors[4], defColors[3], "STT:PlayerColorGreen", "DarkGreen");
-	ReadColor("Slot5", &Colors[5], defColors[4], "STT:PlayerColorOrange", "Orange");
-	ReadColor("Slot6", &Colors[6], defColors[5], "STT:PlayerColorSkyBlue", "DarkSky");
-	ReadColor("Slot7", &Colors[7], defColors[6], "STT:PlayerColorPurple", "Purple");
-	ReadColor("Slot8", &Colors[8], defColors[7], "STT:PlayerColorPink", "Magenta");
+	auto const defColors = reinterpret_cast<int const*>(0x8316A8);
+	ReadColor("Observer", Colors[0], defColors[8], "STT:PlayerColorObserver", "LightGrey");
+	ReadColor("Slot1", Colors[1], defColors[0], "STT:PlayerColorGold", "Gold");
+	ReadColor("Slot2", Colors[2], defColors[1], "STT:PlayerColorRed", "DarkRed");
+	ReadColor("Slot3", Colors[3], defColors[2], "STT:PlayerColorBlue", "DarkBlue");
+	ReadColor("Slot4", Colors[4], defColors[3], "STT:PlayerColorGreen", "DarkGreen");
+	ReadColor("Slot5", Colors[5], defColors[4], "STT:PlayerColorOrange", "Orange");
+	ReadColor("Slot6", Colors[6], defColors[5], "STT:PlayerColorSkyBlue", "DarkSky");
+	ReadColor("Slot7", Colors[7], defColors[6], "STT:PlayerColorPurple", "Purple");
+	ReadColor("Slot8", Colors[8], defColors[7], "STT:PlayerColorPink", "Magenta");
 
 	// additional color schemes so just increasing Count will produce nice colors
-	ReadColor("Slot9", &Colors[9], 0xEF5D94, "STT:PlayerColorLilac", "NeonBlue");
-	ReadColor("Slot10", &Colors[10], 0xE7FF73, "STT:PlayerColorLightBlue", "LightBlue");
-	ReadColor("Slot11", &Colors[11], 0x63EFFF, "STT:PlayerColorLime", "Yellow");
-	ReadColor("Slot12", &Colors[12], 0x5AC308, "STT:PlayerColorTeal", "Green");
-	ReadColor("Slot13", &Colors[13], 0x0055BD, "STT:PlayerColorBrown", "Red");
-	ReadColor("Slot14", &Colors[14], 0x808080, "STT:PlayerColorCharcoal", "Grey");
+	ReadColor("Slot9", Colors[9], 0xEF5D94, "STT:PlayerColorLilac", "NeonBlue");
+	ReadColor("Slot10", Colors[10], 0xE7FF73, "STT:PlayerColorLightBlue", "LightBlue");
+	ReadColor("Slot11", Colors[11], 0x63EFFF, "STT:PlayerColorLime", "Yellow");
+	ReadColor("Slot12", Colors[12], 0x5AC308, "STT:PlayerColorTeal", "Green");
+	ReadColor("Slot13", Colors[13], 0x0055BD, "STT:PlayerColorBrown", "Red");
+	ReadColor("Slot14", Colors[14], 0x808080, "STT:PlayerColorCharcoal", "Grey");
 
 	// blunt stuff
 	char key[0x10];
-	for(int i=15; i<=ColorCount; ++i) {
+	for(auto i = 15; i <= ColorCount; ++i) {
 		sprintf_s(key, 0x10, "Slot%d", i);
-		ReadColor(key, &Colors[i], 0xFFFFFF, "NOSTR:", "LightGrey");
+		ReadColor(key, Colors[i], 0xFFFFFF, "NOSTR:", "LightGrey");
 	}
 
 	// menu colors. the color of labels, button texts, list items, stuff and others
