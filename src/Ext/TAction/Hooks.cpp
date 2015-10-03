7b601246c7b7adb6a41878914dbf35195d863874
AlexB <Alexander-B@gmx.de>
2015-10-03 15:44:06 +0200

    Added auto and const, used an enum for the return values.

--- src/Ext/TAction/Hooks.cpp
+++ src/Ext/TAction/Hooks.cpp
@@ -4,25 +4,27 @@
 
 DEFINE_HOOK(6DD8D7, TActionClass_Execute, A)
 {
-	GET(TActionClass*, pAction, ESI);
-	GET(ObjectClass*, pObject, ECX);
+	GET(TActionClass* const, pAction, ESI);
+	GET(ObjectClass* const, pObject, ECX);
 
-	GET_STACK(HouseClass*, pHouse, 0x254);
-	GET_STACK(TriggerClass*, pTrigger, 0x25C);
-	GET_STACK(CellStruct*, pLocation, 0x260);
+	GET_STACK(HouseClass* const, pHouse, 0x254);
+	GET_STACK(TriggerClass* const, pTrigger, 0x25C);
+	GET_STACK(CellStruct const*, pLocation, 0x260);
+
+	enum { Handled = 0x6DFDDD, Default = 0x6DD8E7u };
 
 	// check for actions handled in Ares.
-	bool ret = false;
+	auto ret = false;
 	if(TActionExt::Execute(
 		pAction, pHouse, pObject, pTrigger, *pLocation, &ret))
 	{
 		// returns true or false
 		R->AL(ret ? 1 : 0);
-		return 0x6DFDDD;
+		return Handled;
 	}
 
 	// replicate the original instructions, using underflow
-	auto value = static_cast<unsigned int>(pAction->ActionKind) - 1;
+	auto const value = static_cast<unsigned int>(pAction->ActionKind) - 1;
 	R->EDX(value);
-	return (value > 144) ? 0x6DFDDD : 0x6DD8E7;
+	return (value > 144u) ? Handled : Default;
 }
