46a3e779a45f3b3546cd32b03256e7cd752e870b
AlexB <Alexander-B@gmx.de>
2015-10-04 20:53:02 +0200

    Changed the AresMap interface
    - erase now returns whether an item was removed
    - insert does not change an existing value, and now return whether the key was inserted

--- src/Utilities/Constructs.h
+++ src/Utilities/Constructs.h
@@ -180,15 +180,12 @@ public:
 		if(auto pValue = this->find(key)) {
 			return *pValue;
 		}
-		this->values.emplace_back(key, TValue());
-		return this->values.back().second;
+		return this->insert_unchecked(key, TValue());
 	}
 
 	TValue* find(const TKey& key) {
-		if(auto pValue = static_cast<const AresMap*>(this)->find(key)) {
-			return const_cast<TValue*>(pValue);
-		}
-		return nullptr;
+		auto pValue = static_cast<const AresMap*>(this)->find(key);
+		return const_cast<TValue*>(pValue);
 	}
 
 	const TValue* find(const TKey& key) const {
@@ -213,19 +210,25 @@ public:
 		return def;
 	}
 
-	void erase(const TKey& key) {
+	bool erase(const TKey& key) {
 		auto it = this->get_iterator(key);
 		if(it != this->values.end()) {
 			this->values.erase(it);
+			return true;
 		}
+		return false;
 	}
 
 	bool contains(const TKey& key) const {
 		return this->get_iterator(key) != this->values.end();
 	}
 
-	void insert(const TKey& key, TValue value) {
-		(*this)[key] = value;
+	bool insert(const TKey& key, TValue value) {
+		if(!this->find(key)) {
+			this->insert_unchecked(key, std::move(value));
+			return true;
+		}
+		return false;
 	}
 
 	size_t size() const {
@@ -278,6 +281,11 @@ private:
 		return std::find_if(this->values.begin(), this->values.end(), [&](const container_t::value_type& item) {
 			return item.first == key;
 		});
+	}
+
+	TValue& insert_unchecked(const TKey& key, TValue value) {
+		this->values.emplace_back(key, std::move(value));
+		return this->values.back().second;
 	}
 
 	container_t values;
